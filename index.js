db79a6d24433f9143c2344dc79f336106d6cb696
Nano <hikarishido1997@gmail.com>
2018-10-03 15:12:41 +0700

    rename variables

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const axios = require('axios'),
-      fs = require('fs'),
-      path = require('path')
+const axios = require('axios')
+const fs = require('fs')
+const path = require('path')
 
 const ID_POST = ''
 const ACCESS_TOKEN = ''
